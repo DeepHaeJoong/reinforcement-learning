63e3053e3209809e67e97d51adaf5f85ce3799ba
DeepHaeJoong <lhj910120@gmail.com>
2021-12-11 17:17:19 +0900

    update
    code 정리1

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # reinforcement-learning
-강화학습 알고리즘 및 코드 구현
+
+강화학습 알고리즘 및 코드 구현 정리
+
+# algorithms
+---
+|  | Paper | Code | Description | Category | 
+|:---:|:---:|:---:|:---:|:---:|
+|1|Q-Learning| [link]() | [link](https://www.notion.so/Q-Learning-4977dad0b52844ec84b81b36268c5da6) |  value-based |
+|2|[DQN](https://arxiv.org/abs/1312.5602)| [link]() | [link](https://www.notion.so/Playing-Atari-with-Deep-Reinforcement-Learning-DQN-037e9d59e9754ffca8d0b49fd1c04d56) |  value-based |
+|3|[double DQN](https://arxiv.org/abs/1509.06461)| [link]() | [link](https://www.notion.so/Double-DQN-9aaa872fb3ae48c0bfd6ca797ec09348) |  value-based |
+|4|[dueling DQN](https://arxiv.org/abs/1511.06581)| [link]() | [link](https://www.notion.so/Dueling-DQN-961fdd86533d42f8965232f735c2a820) |  value-based |
+|5|[REINFORCE](https://proceedings.neurips.cc/paper/1999/file/464d828b85b0bed98e80ade0a5c43b0f-Paper.pdf)| [link]() | [link]() |  value-based |
+|6|[A2C/A3C](https://arxiv.org/abs/1602.01783)| [link-1]()/[link-2]() | [link](https://www.notion.so/Actor-Critic-Methods-99f7d65cd570486982fdbf5f4628d20c) |  policy-based |
+|7|[DDPG]()| [link]() | [link](https://www.notion.so/DDPG-5dddb8beaef449329af6a0b41041d190) | - |
+|8|[TD3]()| [link]() | [link](https://www.notion.so/Twin-Delayed-DDPG-TD3-2be16edc798b4ddba579b5516e6beee3) | - |
+|9|[SAC]()| [link]() | [link](https://www.notion.so/Soft-Actor-Critic-SAC-8facc17d797b4d439dfda202f2c874e1) | - |
+|10|[PPO]()| [link]() | [link](https://www.notion.so/Proximal-Policy-Optimization-PPO-56b4fecb1dd541efb176103ad7e9a3c6) | - |
+|11|[SAC]()| ... | ... | - |
+
+# 
---
+++ dueling_DQN/dueling_DQN.py
@@ -0,0 +1,150 @@
+# https://www.youtube.com/watch?v=__NYgfkUr-M&t=119s
+
+# libraries
+import gym
+import collections
+import random
+
+# pytorch library is used for deep learning
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# hyperparameters
+learning_rate = 0.0005
+gamma = 0.98
+buffer_limit = 50000        # size of replay buffer
+batch_size = 32
+
+
+class ReplayBuffer():
+    def __init__(self):
+        """
+        FIFO
+        """
+        self.buffer = collections.deque(maxlen=buffer_limit)  # double-ended queue
+
+    def put(self, transition):
+        self.buffer.append(transition)
+
+    def sample(self, n):
+        mini_batch = random.sample(self.buffer, n)
+        s_lst, a_lst, r_lst, s_prime_lst, done_mask_lst = [], [], [], [], []
+
+        for transition in mini_batch:
+            s, a, r, s_prime, done_mask = transition
+            s_lst.append(s)
+            a_lst.append([a])
+            r_lst.append([r])
+            s_prime_lst.append(s_prime)
+            done_mask_lst.append([done_mask])
+
+        return torch.tensor(s_lst, dtype=torch.float), torch.tensor(a_lst), \
+               torch.tensor(r_lst), torch.tensor(s_prime_lst, dtype=torch.float), \
+               torch.tensor(done_mask_lst)
+
+    def size(self):
+        return len(self.buffer)
+
+class DuelingQnet(nn.Module):
+    def __init__(self, actions=2):
+        super(DuelingQnet, self).__init__()
+        self.fc1 = nn.Linear(4, 128)
+        self.fc_value = nn.Linear(128, 128)
+        self.fc_adv = nn.Linear(128, 128)
+        self.value = nn.Linear(128, 1)
+        self.adv = nn.Linear(128, actions)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        v = F.relu(self.fc_value(x))
+        a = F.relu(self.fc_adv(x))
+        # value function
+        v = self.value(v)
+        # advantage function
+        a = self.adv(a)
+
+        # Normalize the advantage function
+        a_avg = torch.mean(a)
+        q = v + a - a_avg
+        return q
+
+    def sample_action(self, obs, epsilon):
+        out = self.forward(obs)
+        coin = random.random()
+        if coin < epsilon:
+            return random.randint(0,1)
+        else :
+            return out.argmax().item()
+
+
+def main():
+    env = gym.make('CartPole-v1')
+
+    q = DuelingQnet(actions=2)          # Initialize the main network
+    q_target = DuelingQnet(actions=2)   # Initialize the target network
+    q_target.load_state_dict(q.state_dict()) #q model의 weight 정보를 dictionary 형태로 닮고 있음
+    memory = ReplayBuffer()      # Initialize the replay buffer D
+
+    score = 0
+    time_step = 2000
+    optimizer = torch.optim.Adam(q.parameters(), lr=0.0005)  # the main parameter
+
+    # For N number of epsisodes
+    for n_epi in range(2000):
+        epsilon = max(0.01, 0.08-0.01*(n_epi/200))   # 0.08 -> 0.01
+        state = env.reset()
+
+        for t in range(time_step):
+            # observe the state (s) and select action using epsilon-greedy policy.
+            action = q.sample_action(obs=torch.from_numpy(state).float(), epsilon=epsilon)
+            # perform the selected action and move the next state (s') and obtain the reward (r)
+            next_state, reward, done, info = env.step(action)
+            done_mask = 0.0 if done else 1.0
+            # store the transition information of K (600) transitions from the replay buffer D
+            memory.put((state, action, reward/200.0, next_state, done_mask))
+            state = next_state
+
+            score += reward
+
+            if done:
+                break
+
+        if memory.size() > 2000:
+            # memory 쌓인 이후 학습 시작
+            train(q, q_target, memory, gamma, optimizer, batch_size)
+
+        if n_epi%20 == 0 and n_epi != 0:
+            # Freeze the target network parameter (several time step) and then update it by just copying the main network parameter
+            q_target.load_state_dict(q.state_dict())
+            print("# of episode : {}, score : {:.1f}, buffer_size : {}, epsilon : {:.1f}%".format(n_epi,
+                                                                                                  score/20.0,
+                                                                                                  memory.size(),
+                                                                                                  epsilon*100))
+            score = 0.0
+
+def train(q, q_target, memory, gamma, optimizer, batch_size):
+
+    for i in range(10):
+        # Randomly sample a mini-batch of K transitions from the replay buffer D
+        s, a, r, s_prime, done_mask = memory.sample(batch_size)
+
+        q_out = q(s)
+        q_a = q_out.gather(1, a)  # 취한 action의 q 값만 골라냄. Shape : [32, 1]
+        max_q_prime = q_target(s_prime).max(1)[0].unsqueeze(1)
+        target = r + gamma * max_q_prime * done_mask
+        # Compute the loss and the gradient of the loss and update main
+        # MSE Loss
+        loss = F.mse_loss(q_a, target)
+
+        # Smooth L1 Loss
+        #loss = F.smooth_l1_loss(q_a, target)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+
+if __name__ == '__main__':
+    main()
+
